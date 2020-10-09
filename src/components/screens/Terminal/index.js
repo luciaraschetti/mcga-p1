6b36d88b60a600ef1a2d856965d535b9c35c59f4
lucia raschetti <lucia.sn011@gmail.com>
2020-10-09 03:39:51 -0300

    MCGA-P1: Add Terminal username input

--- src/components/screens/Terminal/index.js
+++ src/components/screens/Terminal/index.js
@@ -3,6 +3,30 @@ import React, { Component } from 'react';
 import Typist from 'react-typist';
 
 export class index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: '',
+            name: '',
+        };
+    }
+
+    show() {
+        this.setState({user: 'user@parcial-01:'});
+    }
+
+    updateValue = (name) => {
+        this.setState({name});
+        console.log(name)
+    }
+
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.show();
+            this.nameInput.focus();
+        }, 4000);
+    }
+
     render() {
         return (
             <div className="terminal">
@@ -10,10 +34,30 @@ export class index extends Component {
                     <p>user@parcial-01:~</p>
                 </div>
                 <div className="terminal-body">
-                    <Typist className="typed" stdTypingDelay={15} startDelay={500}>
-                        <span className="text">Welcome to Parcial-01!</span>
+                    <div className="typed">
+                    <p className="text">Welcome to Parcial-01!</p>
+                    <Typist 
+                        stdTypingDelay={15} 
+                        startDelay={500} 
+                        cursor={ 
+                                    {
+                                        element: '❚',
+                                        hideWhenDone: true, 
+                                        hideWhenDoneDelay: 0, 
+                                    } 
+                                }
+                    >
                         <span className="text">Please enter your first and last name...</span>
                     </Typist>
+                    <span className="text user">{this.state.user}</span> 
+                    <input 
+                        className="user-name" 
+                        type="text"
+                        ref={(input) => {this.nameInput = input;}} 
+                        value={this.state.name}
+                        onChange={(input) => this.updateValue(input.target.value)}
+                    />
+                    </div>
                 </div>
             </div>
         )
